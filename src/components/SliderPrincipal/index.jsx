fad00b25f19ab8a5c89eee75c3339faff2e8e0f7
MoisesSGomes <moises.sgomes@outlook.com>
2025-06-25 16:01:49 -0300

    atualização rota imagem slider

--- src/components/SliderPrincipal/index.jsx
+++ src/components/SliderPrincipal/index.jsx
@@ -5,7 +5,6 @@ import { Autoplay, Navigation } from 'swiper/modules'
 import api from '../../services/api'
 
 function Slider() {
-  const BASE_URL = "https://letsgoparty-api.onrender.com"
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -55,7 +54,7 @@ function Slider() {
             <LinkEvento href={`/evento?id=${item.id}`}>
               <SliderContainer>
                 <SwiperDiv1>
-                  <ImgItem src={`${BASE_URL}${item.image}`} alt={item.titleEvent} />
+                  <ImgItem src={item.image} alt={item.titleEvent} />
                 </SwiperDiv1>
                 <SwiperDiv2>
                   <h3>{item.titleEvent}</h3>
